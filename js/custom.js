1e8b789f05f2e4634330d97b8dbf4a60c584cee9
Matt Le Fleur <mlefleur@googlemail.com>
2014-05-08 13:58:16 +0100

    Edit navigation bar

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 // JavaScript Document
 
 $(document).ready(function()	{
-	$('#lorem').load('lorem10.html');
 	$('#navbar').load('nav.html');
 	$('#footerContainer_1').load('footer.html');
 	$('#username').load('username.html');
